7b9ca1659900e29cc16f6c614f2703310c1e67f6
Akshath Jain <akshath.r.jain@gmail.com>
2019-03-26 17:54:25 -0400

    finished app

--- foxtrot_app/lib/main.dart
+++ foxtrot_app/lib/main.dart
@@ -1,4 +1,12 @@
+/*
+Name: Akshath Jain
+Date: 3/26/19
+Purpose: gets the status of the door
+*/
+
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 void main() => runApp(Foxtrot());
 
@@ -9,7 +17,8 @@ class Foxtrot extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.black,
+        accentColor: Colors.red
       ),
       home: HomePage(),
     );
@@ -23,13 +32,102 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   
+  Map _doorStatus;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _fetchData();
+  }
+
   @override
   Widget build(BuildContext context) {
+
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Foxtrot"),
       ),
-      body: Container(),// This trailing comma makes auto-formatting nicer for build methods.
+      body: _body(),
     );
+  }
+
+  Widget _body(){
+    if(_doorStatus == null)
+      return Center(child: CircularProgressIndicator(),);
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+
+        Center(
+          child: Container(
+            width: 175,
+            height: 175,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(
+                color: _getColor(),
+                width: 6.0
+              ),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Icon(
+                  _getIcon(),
+                  color: _getColor(),
+                  size: 75.0,
+                ),
+              ],
+            ),
+          ),
+        ),
+        
+
+        SizedBox(height: 24.0,),
+
+        Text(
+          _getString(),
+          style: TextStyle(
+            fontSize: 18.0
+          ),
+        ),
+
+        SizedBox(height: 48.0,),
+
+        RaisedButton(
+          child: Text("Check Status"),
+          onPressed: _fetchData,
+          color: Colors.black,
+          textColor: Colors.white,
+        ),
+      ],
+    );
+  }
+
+  Color _getColor(){
+    return _doorStatus["doorOpen"] == false ? Colors.green : Colors.red;
+  }
+
+  IconData _getIcon(){
+    return _doorStatus["doorOpen"] == false ? Icons.check : Icons.clear;
+  }
+
+  String _getString(){
+    return _doorStatus["doorOpen"] == false ? "Door Closed " : "Door Open";
+  }
+
+  void _fetchData() async{
+    var response = await http.get("http://128.237.209.19");
+
+    if(mounted){
+      setState(() {
+        this._doorStatus = json.decode(response.body);
+      });
+    }
   }
 }
